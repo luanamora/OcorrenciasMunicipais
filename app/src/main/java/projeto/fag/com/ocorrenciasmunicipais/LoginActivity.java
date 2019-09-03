19b28a1b78174c17f1f7f82c3eb74d292155d465
Luana Mora <luanamora32@gmail.com>
2019-09-03 15:18:12 -0300

    Atualizações de tela de Login.

--- app/src/main/java/projeto/fag/com/ocorrenciasmunicipais/LoginActivity.java
+++ app/src/main/java/projeto/fag/com/ocorrenciasmunicipais/LoginActivity.java
@@ -2,11 +2,13 @@ package projeto.fag.com.ocorrenciasmunicipais;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText etEmail, etSenha;
+    private Button btEntrar, btCriarNovaConta, btEsqueceuSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     private void loadComponents(){
         etEmail = findViewById(R.id.etEmail);
         etSenha = findViewById(R.id.etSenha);
+        btEntrar = findViewById(R.id.btEntrar);
+        btCriarNovaConta = findViewById(R.id.btCriarNovaConta);
+        btEsqueceuSenha = findViewById(R.id.btEsqueceuSenha);
     }
 }
