494e2d88b8d5762a95a1c4a043640dfda39077df
Luana Mora <luanamora32@gmail.com>
2019-09-03 14:38:04 -0300

    Adicionado Icones.

--- app/src/main/java/projeto/fag/com/ocorrenciasmunicipais/LoginActivity.java
+++ app/src/main/java/projeto/fag/com/ocorrenciasmunicipais/LoginActivity.java
@@ -2,12 +2,24 @@ package projeto.fag.com.ocorrenciasmunicipais;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail, etSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loadComponents();
+
+
+    }
+
+    private void loadComponents(){
+        etEmail = findViewById(R.id.etEmail);
+        etSenha = findViewById(R.id.etSenha);
     }
 }
