fdade71d8d7a8a98caef02d2691d5c5b4fc1beef
Luana Mora <luanamora32@gmail.com>
2019-10-16 22:09:05 -0300

    Task Genérico Get

--- OcorrenciasMunicipais/app/src/main/java/projeto/fag/com/ocorrenciasmunicipais/task/Task.java
+++ OcorrenciasMunicipais/app/src/main/java/projeto/fag/com/ocorrenciasmunicipais/task/Task.java
@@ -1,23 +1,31 @@
 package projeto.fag.com.ocorrenciasmunicipais.task;
 
+import android.content.Context;
 import android.os.AsyncTask;
 
 import java.io.BufferedOutputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Task extends AsyncTask<String, Integer, Result> {
+
+    private Context context;
+
+    public Task(Context context) {
+        this.context = context;
+    }
 
     @Override
     protected Result doInBackground(String... params) {
         if (params != null && params.length > 0) {
             try {
                 StringBuffer response = new StringBuffer();
-                URL urlResult = new URL("http://192.168.1.61:5000/api/" + params[0]);
+                URL urlResult = new URL("http://192.168.43.154:5000/api/" + params[0]);
                 HttpURLConnection connection = (HttpURLConnection) urlResult.openConnection();
-                connection.setRequestMethod("Get");
+                connection.setRequestMethod("GET");
                 connection.setRequestProperty("Content-Type", "application/json");
                 connection.setRequestProperty("Accept", "application/json");
                 connection.setConnectTimeout(20000);
@@ -29,9 +37,12 @@ public class Task extends AsyncTask<String, Integer, Result> {
                     Scanner scanner = new Scanner(connection.getInputStream());
                     while (scanner.hasNext()) {
                         response.append(scanner.next());
+
                     }
+                    System.out.println("Entrou aqui no 1");
                     return new Result(response.toString(), false);
                 } else
+                    System.out.println("Entrou aqui no 2");
                     return new Result(null, true);
 
             } catch (Exception ex) {
