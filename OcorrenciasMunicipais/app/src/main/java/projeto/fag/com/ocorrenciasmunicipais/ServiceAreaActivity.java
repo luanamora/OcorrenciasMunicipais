1577b08a2bc35dffdfa48bd1aa004fff2918aa5a
Luana Mora <luanamora32@gmail.com>
2019-10-16 21:26:14 -0300

    Atualizações

---
+++ OcorrenciasMunicipais/app/src/main/java/projeto/fag/com/ocorrenciasmunicipais/ServiceAreaActivity.java
@@ -0,0 +1,13 @@
+package projeto.fag.com.ocorrenciasmunicipais;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_area);
+    }
+}
