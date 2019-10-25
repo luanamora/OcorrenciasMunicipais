509726a5bad8b430e8a1b6d492ec1c52b3b847e8
Luana Mora <luanamora32@gmail.com>
2019-10-25 12:59:52 -0300

    Atualizacao

---
+++ OcorrenciasMunicipais/app/src/main/java/projeto/fag/com/ocorrenciasmunicipais/adapter/AdminUserAdapter.java
@@ -0,0 +1,63 @@
+package projeto.fag.com.ocorrenciasmunicipais.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+import projeto.fag.com.ocorrenciasmunicipais.R;
+import projeto.fag.com.ocorrenciasmunicipais.model.Usuario;
+
+public class AdminUserAdapter extends BaseAdapter {
+    LayoutInflater myInflater;
+    List<Usuario> usuarioList;
+
+    public AdminUserAdapter(Context context, List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+        myInflater = LayoutInflater.from(context); //Responsavel por inflar o layout
+    }
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        Usuario usuario = usuarioList.get(position);
+        view = myInflater.inflate(R.layout.item_admin, null);
+        ((Spinner) view.findViewById(R.id.spAdminUser)).setTe
+
+        return null;
+    }
+}
+
+/*
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        Corretor corretor = corretorList.get(position);
+        view = myInflater.inflate(R.layout.item_imovel, null);
+        ((TextView) view.findViewById(R.id.etCodigo)).setText(String.valueOf(corretor.getCodigo()));
+        ((TextView) view.findViewById(R.id.etNome)).setText(String.valueOf(corretor.getNome()));
+        ((TextView) view.findViewById(R.id.etTelefone)).setText(String.valueOf(corretor.getTelefone()));
+        ((TextView) view.findViewById(R.id.etCreci)).setText(String.valueOf(corretor.getCreci()));
+
+        return view;
+    }
+*/
